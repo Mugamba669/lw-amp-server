1e88a65a7f094fbeaafa1f01cf8734dbc71b4d49
Mugamba669 <brunohectre@gmail.com>
2022-02-21 19:32:11 +0300

    Feature =>  added home ui

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.blueGrey,
       ),
       home: const Splash(),
     );
--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:lw_server/helpers/Global.dart';
 import 'package:lw_server/models/Server.dart';
 
 class Home extends StatefulWidget {
@@ -12,8 +15,65 @@ class _HomeState extends State<Home> {
   ///  Server server = Server(404).getInstance;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Center(child: Text("Welcome"))),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[500],
+      appBar: AppBar(
+        toolbarHeight: size.height / 5,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        shadowColor: Colors.transparent,
+        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 26),
+        title: const Text("Establish connection"),
+      ),
+      body: SafeArea(
+        child: Container(
+          width: size.width,
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50), topRight: Radius.circular(50))),
+          child: Column(
+            children: [
+              // ignore: prefer_const_constructors
+              Padding(
+                padding: EdgeInsets.only(top: size.height / 4),
+                child: Text(
+                  "Type the address below in your desktop client",
+                  style: Theme.of(context).textTheme.headline6!.copyWith(
+                        fontSize: 18,
+                      ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: size.height / 10),
+                child: Text(
+                  "Server running on $ADDRESS:$PORT",
+                  style: Theme.of(context).textTheme.headline6!.copyWith(
+                        fontSize: 18,
+                      ),
+                ),
+              ),
+              Center(
+                  child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Connect",
+                  // style: Theme.of(context).textTheme.headline5,
+                ),
+              )),
+            ],
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+                accountName: Text("Trial"), accountEmail: Text("data"))
+          ],
+        ),
+      ),
     );
   }
 }
